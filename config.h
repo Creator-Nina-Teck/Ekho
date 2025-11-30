9c9652de2cdd571c1fd161113e6b8a9c772e667c
Creator-Nina-Teck <yatech10@gmail.com>
2025-11-30 21:48:17 +0100

    Add configuration header file with constants

---
+++ config.h
@@ -0,0 +1,21 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+
+#define BTN_RECORD 4
+#define BTN_PLAY 5
+#define BTN_RANDOM 6
+
+
+#define I2S_WS 7
+#define I2S_SD 8
+#define I2S_SCK 9
+
+
+#define SPK_PIN 10
+
+
+#define MAX_CLIPS 20
+#define MAX_DURATION_SEC 12
+
+#endif
